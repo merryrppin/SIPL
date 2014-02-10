7fb10618715d808ea1a6d10415bb8da00eaac44e
WM <WM@Wilmar>
2014-02-09 20:00:57 -0500

    creación del paquete dominio y la clase Usuario

--- src/java/sipl/dominio/Danho.java
+++ src/java/sipl/dominio/Danho.java
@@ -6,10 +6,74 @@
 
 package sipl.dominio;
 
+import java.util.Calendar;
+
 /**
  *
  * @author Samy
  */
 public class Danho {
-    
+    private int codigo;
+    private String descripcion;
+    private Material mat;
+    private Usuario usu;
+    private Calendar fecha_d;
+    private Usuario usu_rd;
+
+    public Danho(int codigo, String descripcion, Material mat, Usuario usu, Calendar fecha_d, Usuario usu_rd) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.mat = mat;
+        this.usu = usu;
+        this.fecha_d = fecha_d;
+        this.usu_rd = usu_rd;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Material getMat() {
+        return mat;
+    }
+
+    public void setMat(Material mat) {
+        this.mat = mat;
+    }
+
+    public Usuario getUsu() {
+        return usu;
+    }
+
+    public void setUsu(Usuario usu) {
+        this.usu = usu;
+    }
+
+    public Calendar getFecha_d() {
+        return fecha_d;
+    }
+
+    public void setFecha_d(Calendar fecha_d) {
+        this.fecha_d = fecha_d;
+    }
+
+    public Usuario getUsu_rd() {
+        return usu_rd;
+    }
+
+    public void setUsu_rd(Usuario usu_rd) {
+        this.usu_rd = usu_rd;
+    }
 }
--- src/java/sipl/dominio/Prestamo.java
+++ src/java/sipl/dominio/Prestamo.java
@@ -6,10 +6,66 @@
 
 package sipl.dominio;
 
+import java.util.Calendar;
+
 /**
  *
  * @author Samy
  */
 public class Prestamo {
+    private int codigo;
+    private Material mat;
+    private Usuario usu;
+    private Calendar fecha_prestamo;
+    private Calendar fecha_devolucion;
+
+    public Prestamo(int codigo, Material mat, Usuario usu, Calendar fecha_prestamo, Calendar fecha_devolucion) {
+        this.codigo = codigo;
+        this.mat = mat;
+        this.usu = usu;
+        this.fecha_prestamo = fecha_prestamo;
+        this.fecha_devolucion = fecha_devolucion;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Material getMat() {
+        return mat;
+    }
+
+    public void setMat(Material mat) {
+        this.mat = mat;
+    }
+
+    public Usuario getUsu() {
+        return usu;
+    }
+
+    public void setUsu(Usuario usu) {
+        this.usu = usu;
+    }
+
+    public Calendar getFecha_prestamo() {
+        return fecha_prestamo;
+    }
+
+    public void setFecha_prestamo(Calendar fecha_prestamo) {
+        this.fecha_prestamo = fecha_prestamo;
+    }
+
+    public Calendar getFecha_devolucion() {
+        return fecha_devolucion;
+    }
+
+    public void setFecha_devolucion(Calendar fecha_devolucion) {
+        this.fecha_devolucion = fecha_devolucion;
+    }
+    
     
 }
